f2bf419516abc81874ed508f0e27b10e1a3c2abd
crabaqueropi <crabaqueropi@unal.edu.co>
2021-12-08 14:55:23 -0500

    first version - rasterization algorithm

---
+++ content/docs/Workshops/2. Rendering/rasterization_algorithm.md
@@ -0,0 +1,4 @@
+# Rendering
+
+{{< p5-iframe sketch="/vc/workshops/Rasterization/zBuffer4.js" lib1="https://cdn.jsdelivr.net/gh/freshfork/p5.EasyCam@1.2.1/p5.easycam.js" width="500" height="500" >}}
+
---
+++ content/workshops/Rasterization/zBuffer4.js
@@ -0,0 +1,43 @@
+let W;
+let H;
+
+var near, far;
+
+let myShader;
+
+function preload() {
+  myShader = loadShader("/vc/docs/sketches/rendering/shaderOriginal.vert", "/vc/docs/sketches/rendering/depthmap.frag");
+}
+
+function setup() {
+  W = 500;
+  H = 500;
+  createCanvas(W, H, WEBGL);
+  createEasyCam();
+
+  shader(myShader);
+  near = 1;
+  far = 800;
+  myShader.setUniform('near', near);
+  myShader.setUniform('far', far);
+}
+
+function draw() {
+  // projection
+  perspective(60 * PI/180, width/height, near, far);
+
+  // clear BG
+  background(255);
+  noStroke();
+
+  fill(255, 0, 0)
+  translate(-150, 150, 0);
+  box(100)
+  fill(0, 0, 255)
+  translate(150, -150, 0);
+  box(100)
+  fill(0, 255, 0)
+  translate(150, -150, 0);
+  box(100)
+ 
+}
